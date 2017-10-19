26ac7b1ef3f40bae3212dddc34972e59f22e7767
ncuwwq <32818658+ncuwwq@users.noreply.github.com>
2017-10-19 23:51:48 +0800

    Update work.py
    修改后

--- work.py
+++ work.py
@@ -1,15 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 def get():
-    try:
         r=requests.get('http://news.ncu.edu.cn/')
         r.encoding='utf-8'
         demo=r.text
         soup=BeautifulSoup(demo,'html.parser')
-        x=soup.find_all('title')
-        print(x)
-    except:
-        print("爬取失败")
-
+        for news in soup.select('.newslist')
+            x=soup.select('ul')
+            print(x)
 get()
 
